321e0c84b87ac9b48190aa18eed71e4462111a9d
Shashi Thipparthy <shashi_thipparthy@gallup.com>
2019-01-13 16:50:59 -0600

    sucessfully invoked lamda

--- handler.js
+++ handler.js
@@ -1,26 +1,10 @@
-'use strict';
-const serverless = require('serverless-http');
-const app=require('./server-build/bundle').app;
-//const app=require('./server-build/bundle');
-module.exports.hello = async (event, context) => {
-  const handler = serverless(app);
-module.exports.handler = async (event, context) => {
-    console.log(" in the serverless call event",event);
-    console.log(" in the serverless call event",context);
-
-  return await handler(event, context);
-};
-
-
-  return {
-
+module.exports.hello = (event, context, callback) => {
+  const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      message: 'Your function executed successfully!'
+    })
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, response);
 };
